337c2a79f6f709af8547af7dcd5f02efb05a03c5
shriramt124 <shriramt.124@gmail.com>
2024-03-06 01:12:01 +0530

    git dummy pages are added and routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
- 
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
 function App() {
-  
-
   return (
-    <>
-       <h1 className="text-3xl bg-blue-600 ">hello world</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
