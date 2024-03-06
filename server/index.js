19076cc28b1e2a91fea05483f5f1bc3f5e899e2e
shriramt124 <shriramt.124@gmail.com>
2024-03-07 00:44:19 +0530

    created user model

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import  dotenv from 'dotenv';
+ 
 const app = express();
+dotenv.config();
+ 
+const dbConnect  = async ()=>{
+    await mongoose.connect(process.env.MONGO_URL)
+    .then(()=>console.log("db connected.."))
+    .catch((error)=>console.log(error))
+ }
+ 
 
-
-const PORT  = process.env.PORT || 300
+const PORT  = process.env.PORT || 3000
 app.listen(PORT , ()=>{
+   dbConnect();
+    
     console.log('server start at port '+ PORT);
 })
